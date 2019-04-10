8e8863093f130457b3c521ed2db7c47ce7cc424a
lin <test@test.com>
2019-04-10 13:59:47 +0800

    Todo 样式

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,6 +1,21 @@
 <template>
   <section>
-    <el-checkbox v-model="checked">待办项1</el-checkbox>
+    <el-input placeholder="请输入内容" v-model="todo">
+      <template slot="prepend">
+        <div style="width: 100px;">
+          <el-checkbox v-model="checked"></el-checkbox>
+          <el-select v-model="select" slot="prepend" placeholder="请选择" style="width: 90px;">
+            <el-option label="林xx" value="1"></el-option>
+            <el-option label="王xx" value="2"></el-option>
+            <el-option label="张xx" value="3"></el-option>
+          </el-select>
+        </div>
+      </template>
+      <template slot="append">
+        <i class="el-icon-edit" style="padding-right: 10px;"></i>
+        <i class="el-icon-delete"></i>
+      </template>
+    </el-input>
   </section>
 </template>
 
@@ -8,7 +23,9 @@
 export default {
   data () {
     return {
-      checked: true
+      checked: true,
+      select: '',
+      todo: '某某任务'
     }
   }
 }
